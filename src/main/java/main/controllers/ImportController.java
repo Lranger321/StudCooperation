4a8226fa8cf418b029b41410a689dc1fa6360fd9
Nikita Kozlov <nkozlov@vp.ru>
2022-01-20 00:38:18 +0600

    Import

--- src/main/java/main/controllers/ImportController.java
+++ src/main/java/main/controllers/ImportController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import main.service.ConverterService;
 import org.apache.poi.util.IOUtils;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,10 +20,12 @@ public class ImportController {
 
     private final ConverterService service;
 
-    @PostMapping(value = "/api/import/",   produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public @ResponseBody byte[] importFile(@RequestParam MultipartFile[] files) throws IOException {
+    @PostMapping(value = "/api/import/", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> importFile(@RequestParam MultipartFile[] files) throws IOException {
         File file = service.convertFiles(convertMultiPartToFile(files));
-        return IOUtils.toByteArray(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=result.xlsx")
+                .body(IOUtils.toByteArray(new FileInputStream(file)));
     }
 
     private List<File> convertMultiPartToFile(MultipartFile[] files) {
